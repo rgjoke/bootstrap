63245996a08fb12d127bb7e3e023fb4cb45ffbc7
rgjoke <r.darktemptation@gmail.com>
2020-04-23 00:50:09 +0600

    Commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 $('#blogCarousel').carousel({
     interval: 5000
 });
+
+var menu = $("#navbar");
+var logo = $('.navbar-brand');
+$(window).scroll(function() {
+  var top = $(this).scrollTop();
+  if ( top > 20 ) {
+    menu.css({background: 'rgba(0, 0, 0, 0.7)'});
+    logo.css({opacity: 1});
+  } else if (top < 20) {
+      menu.css({background: 'none'});
+  }
+});
